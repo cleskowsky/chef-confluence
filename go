644e55bcc71a747f84a234b0dd93ad5cc53365c8
christian <christian@confluence-virtualbox.(none)>
2012-12-01 09:15:47 -0500

    added mysql::server, openssl

---
+++ go
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo chef-solo -c confluence-solo.rb -j confluence-solo.json
--- roles/confluence.rb
+++ roles/confluence.rb
@@ -1,11 +1,10 @@
 name "confluence"
 description "confluence role"
 run_list(
-	"recipe[mysql]"
+	"recipe[mysql::server]"
 )
-default_attributes
-	"mysql" => {
-		"server_debian_password" => "confluence",
-		"server_repl_password" => "confluence",
-		"server_root_password" => "confluence"
-	}
+default_attributes "mysql" => {
+	"server_debian_password" => "confluence",
+	"server_repl_password" => "confluence",
+	"server_root_password" => "confluence"
+}
